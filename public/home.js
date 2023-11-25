f8f5542810fb78da108407cd1d7fe6f48f74c874
Shivam <shivam66jnp@gmail.com>
2023-11-25 07:39:28 +0530

    last commit 3

--- public/home.js
+++ public/home.js
@@ -1,29 +1,32 @@
 // 1v1 Drawing and Guessing Game Instructions
 const instructions1v1 = [
-    "1. Welcome to the 1v1 Drawing and Guessing Game!",
-    "2. Admin Privileges: As the game creator (admin), design both public and private games.",
-    "3. Public vs. Private: Public games are open to anyone; private games require a specific link.",
-    "4. Your Turn to Draw: Illustrate the given word or phrase as the drawer.",
-    "5. Switch Roles: Alternate roles with your opponent after each round.",
-    "6. Communication is Key: Use the in-game chat to communicate.",
-    "7. Strive for Excellence: Showcase your artistic skills for victory!",
-    "8. Enjoy the Challenge: Embrace the drawing and guessing experience.",
-    "9. Chat Interaction: Engage in friendly banter during the game.",
-    "10. Have Fun: Enjoy the 1v1 drawing game experience!",
+    "- Decide who will be part of your DrawNPlay.",
+    "- Share the game link with your friends whom you want to play with.",
+    "- Game Setup:",
+    "1) Choose your game pool, i.e. Public or Private game room.",
+    "2) Generate a game link and share it with whosoever you want!",
+    "3) Story your art: Choose a word and create a story through drawings and captions.",
+    "4) Guess the drawing: Guess the word behind each others drawings.",
+    "5) Feel free to save any drawing you like.",
+    "- Drawing Tools: Familiarize yourself with the drawing tools available. These include brush, colors, shapes and an eraser.",
+    "- Interaction: Use the chat to communicate with other participants. Share thoughts about drawings, make guesses, or simply chat while playing.",
+    "- Enjoy the Experience: Most importantly, have fun! Whether you're playing with friends, family, or colleagues, your personalized DrawNPlay Game is designed for enjoyment and creativity.",
 ];
 
 // Multiplayer Drawing Game Instructions
 const instructionsMultiplayer = [
-    "1. Welcome to the Multiplayer Drawing Game!",
-    "2. Game Selection: Browse ongoing public games on the platform.",
-    "3. Join Existing Games: Choose a game and join the drawing excitement.",
-    "4. Interact with Players: Engage with participants through the in-game chat.",
-    "5. Make New Friends: Connect with players and share your love for drawing.",
-    "6. Collaborative Drawing: Experience the joy of collaborative drawing.",
-    "7. Express Creativity: Unleash your creativity on the digital canvas.",
-    "8. Unique Challenges: Encounter drawing challenges set by game hosts.",
-    "9. Global Participation: Join games with participants worldwide.",
-    "10. Enjoy the Experience: Immerse yourself, have fun, and create lasting memories.",
+    "- No friend online? Don't worry DrawNPlay got you.",
+    "- Join a random party and showcase your art and guesing skills.",
+    "- Game Setup:",
+    "1) Choose one of the many games currently live.",
+    "2) Draw and guess based on prompts.",
+    "3) Story your art: Create a story through drawings and captions.",
+    "4) Guess the drawing: Participants guess the meaning behind each others drawings.",
+    "5) Feel free to save any drawing you like.",
+    "- Drawing Tools: Familiarize yourself with the drawing tools available. These may include various brushes, colors, and an eraser. Get creative!",
+    "- Interaction: Use the chat or comment features to communicate with other participants. Share thoughts about drawings, make guesses, or simply chat while playing.",
+    "- Enjoy the Experience: Most importantly, have fun! Whether you're playing with friends, family, or colleagues, your personalized Scribble Game is designed for enjoyment and creativity.",
+    "- Maintain decency and avoid the use of foul language or hate symbols.",
 ];
 
 
